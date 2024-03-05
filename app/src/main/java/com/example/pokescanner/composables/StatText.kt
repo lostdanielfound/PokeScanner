61b11c2940d0c87c09ce8ae9965b598aed8bbeec
Daniel Guzman <danielsonguzman2000@gmail.com>
2024-03-05 13:55:58 -0800

    Added dark mode capabilities

--- app/src/main/java/com/example/pokescanner/composables/StatText.kt
+++ app/src/main/java/com/example/pokescanner/composables/StatText.kt
@@ -1,5 +1,6 @@
 package com.example.pokescanner.composables
 
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -7,6 +8,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 
@@ -22,11 +24,13 @@ fun StatText(text: String, modifier: Modifier = Modifier, statInt: Int = -1) {
             text,
             fontSize = 20.sp,
             fontWeight = FontWeight.Light,
+            color = if (isSystemInDarkTheme()) Color.White else Color.Black
         )
         Text(
             if (statInt == -1) "#" else statInt.toString(),
             fontSize = 24.sp,
-            fontWeight = FontWeight.ExtraBold
+            fontWeight = FontWeight.ExtraBold,
+            color = if (isSystemInDarkTheme()) Color.White else Color.Black
         )
     }
 }
