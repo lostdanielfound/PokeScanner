8a23b62a4a98156f9b7b9025758d932e478e3905
Daniel Guzman <danielsonguzman2000@gmail.com>
2024-03-05 13:54:15 -0800

    Added navigation button for testing

--- app/src/main/java/com/example/pokescanner/screens/HomeScreen.kt
+++ app/src/main/java/com/example/pokescanner/screens/HomeScreen.kt
@@ -1,12 +1,32 @@
 package com.example.pokescanner.screens
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Composable
-fun HomeScreen(modifier: Modifier = Modifier) {
+fun HomeScreen(onNavigateToStats: () -> Unit, modifier: Modifier = Modifier) {
     //Column(
     ////Camera view here
 
     //)
+
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(12.dp)
+    ) {
+        Button(onClick = { onNavigateToStats() }) {
+            Text("Navigate to State screen")
+        }
+    }
 }
