9590a53dce18eaf26692498fab98d8132c5cc0cd
Daniel Guzman <danielsonguzman2000@gmail.com>
2024-03-06 20:08:40 -0800

    add journalScreen Composable to navgraph

--- app/src/main/java/com/example/pokescanner/MainActivity.kt
+++ app/src/main/java/com/example/pokescanner/MainActivity.kt
@@ -13,6 +13,7 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.pokescanner.screens.HomeScreen
+import com.example.pokescanner.screens.JournalScreen
 import com.example.pokescanner.screens.StatsScreen
 import com.example.pokescanner.ui.theme.PokeScannerTheme
 
@@ -38,8 +39,9 @@ class MainActivity : ComponentActivity() {
                     val navController = rememberNavController()
 
                     NavHost(navController = navController, startDestination = "home") {
-                        composable("home") { HomeScreen( { navController.navigate("stats") } ) }
+                        composable("home") { HomeScreen( { navController.navigate("journal") } ) }
                         composable("stats") { StatsScreen(currentStats) }
+                        composable("journal") { JournalScreen() }
                     }
                 }
             }
