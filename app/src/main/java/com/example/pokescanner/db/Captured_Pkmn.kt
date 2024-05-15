833e5408fdfa8e6ca8f7387f444e5a8f82b6758e
Daniel Guzman <daniellostsilver2000@gmail.com>
2024-05-15 10:36:42 -0700

    Made changess to db

--- app/src/main/java/com/example/pokescanner/db/Captured_Pkmn.kt
+++ app/src/main/java/com/example/pokescanner/db/Captured_Pkmn.kt
@@ -7,17 +7,10 @@ import androidx.room.PrimaryKey
 import java.time.LocalDateTime
 import kotlin.reflect.KClass
 
-@Entity(
-    foreignKeys = [ForeignKey(
-        entity = Pokemon::class,
-        parentColumns = arrayOf("pkmnID"),
-        childColumns = arrayOf("pkmn"),
-        onDelete = ForeignKey.CASCADE
-    )]
-)
+@Entity
 data class Captured_Pkmn(
     @PrimaryKey(autoGenerate = true) val captured_ID: Int,
-    @ColumnInfo(name = "pkmn") val pkmn: String,
+    @ColumnInfo(name = "pkmnfk_ID") val pkmn: Int,
     @ColumnInfo(name = "capture_time") val captured_time: LocalDateTime,
     @ColumnInfo(name = "image_base64") val image_base64: String
 )
--- app/src/main/java/com/example/pokescanner/db/Captured_PkmnDao.kt
+++ app/src/main/java/com/example/pokescanner/db/Captured_PkmnDao.kt
@@ -4,16 +4,11 @@ import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
+import androidx.room.Query
 import androidx.room.Update
 
 @Dao
 interface Captured_PkmnDao {
-    @Insert(onConflict = OnConflictStrategy.IGNORE)
-    suspend fun insert(pkmn: Pokemon)
-
-    @Update
-    suspend fun update(updatePkmn: Pokemon)
-
-    @Delete
-    suspend fun delete(pkmn: Pokemon)
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(record: Captured_Pkmn)
 }
