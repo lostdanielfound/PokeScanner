333174e69cf6022bcaa7a61541f6d5b727b71a6c
Daniel Guzman <daniellostsilver2000@gmail.com>
2024-05-23 14:56:49 -0700

    Created Database connection

---
+++ app/src/main/java/com/example/pokescanner/db/CapturedPkmnDatabase.kt
@@ -0,0 +1,36 @@
+package com.example.pokescanner.db
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [CapturedPkmn::class], version = 1, exportSchema = true)
+abstract class CapturedPkmnDatabase: RoomDatabase() {
+    abstract fun capturedPkmnDao(): CapturedPkmnDao
+
+    companion object{
+        @Volatile //Creating an atomic Instance of the database
+        private var INSTANCE: CapturedPkmnDatabase? = null
+
+        //Function to get and start building process of database
+        //If instance variable is already set with current state
+        //of database we will return that since building a database
+        //is quite expensive for performance.
+        fun getDatabase(context: Context): CapturedPkmnDatabase{
+            val tempInstance = INSTANCE
+            if (tempInstance != null) {
+                return tempInstance
+            }
+            synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    CapturedPkmnDatabase::class.java,
+                    "CapturedPkmn_database"
+                ).build()
+                INSTANCE = instance
+                return instance
+            }
+        }
+    }
+}
