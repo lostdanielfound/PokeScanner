c15c9a0fcbc6deb8529a68f91d12de8c68d774e0
Daniel Guzman <daniellostsilver2000@gmail.com>
2024-05-23 14:56:28 -0700

    Alter readAll query and name

--- app/src/main/java/com/example/pokescanner/db/CapturedPkmnDao.kt
+++ app/src/main/java/com/example/pokescanner/db/CapturedPkmnDao.kt
@@ -7,10 +7,10 @@ import androidx.room.OnConflictStrategy
 import androidx.room.Query
 
 @Dao
-interface Captured_PkmnDao {
+interface CapturedPkmnDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insert(record: CapturedPkmn)
 
-    @Query("SELECT * FROM CapturedPkmn")
-    suspend fun readAll(): LiveData<List<CapturedPkmn>>
+    @Query("SELECT * FROM capturedPkmn_table ORDER BY capturedID ASC")
+    fun readAll(): LiveData<List<CapturedPkmn>>
 }
