dfc49ee49868dae9c438a5961a9ad79db8e7f50d
Daniel Guzman <daniellostsilver2000@gmail.com>
2024-05-23 02:40:05 -0700

    redirect stats data

--- app/src/main/java/com/example/pokescanner/screens/AppUiState.kt
+++ app/src/main/java/com/example/pokescanner/screens/AppUiState.kt
@@ -1,7 +1,5 @@
 package com.example.pokescanner.screens
 
-import com.example.pokescanner.composables.Stats
-
 data class AppUiState(
     val stats: Stats
 )
--- app/src/main/java/com/example/pokescanner/screens/AppViewModel.kt
+++ app/src/main/java/com/example/pokescanner/screens/AppViewModel.kt
@@ -1,7 +1,6 @@
 package com.example.pokescanner.screens
 
 import androidx.lifecycle.ViewModel
-import com.example.pokescanner.composables.Stats
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
