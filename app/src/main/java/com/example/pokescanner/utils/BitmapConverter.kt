993c9a8c86b585b5d53c6016e4fd9d2e29d167e3
Daniel Guzman <danielsonguzman2000@gmail.com>
2024-05-24 05:01:35 -0400

    converter requires blob parameter

--- app/src/main/java/com/example/pokescanner/utils/BitmapConverter.kt
+++ app/src/main/java/com/example/pokescanner/utils/BitmapConverter.kt
@@ -4,6 +4,7 @@ import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.util.Base64
 import java.io.ByteArrayOutputStream
+import java.sql.Blob
 
 object BitmapConverter {
     fun converterBitmapToString(bitmap: Bitmap): String {
@@ -13,9 +14,9 @@ object BitmapConverter {
         return Base64.encodeToString(byteArray, Base64.DEFAULT)
     }
 
-    fun converterStringToBitmap(encodedString: String): Bitmap? {
+    fun converterStringToBitmap(encodedString: Blob): Bitmap? {
         return try {
-            val encodeByte = Base64.decode(encodedString, Base64.DEFAULT)
+            val encodeByte = Base64.decode(encodedString.toString(), Base64.DEFAULT)
             BitmapFactory.decodeByteArray(encodeByte, 0, encodeByte.size)
         } catch (e: Exception) {
             e.printStackTrace()
