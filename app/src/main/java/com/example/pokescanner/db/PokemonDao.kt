f5cfcc52a60642494505ab2418c9b4a160a162f1
Daniel Guzman <danielsonguzman2000@gmail.com>
2024-05-23 23:10:54 -0400

    Took away unneeded methods

--- app/src/main/java/com/example/pokescanner/db/PokemonDao.kt
+++ app/src/main/java/com/example/pokescanner/db/PokemonDao.kt
@@ -1,19 +1,15 @@
 package com.example.pokescanner.db
 
+import androidx.lifecycle.LiveData
 import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
+import androidx.room.Query
 import androidx.room.Update
 
 @Dao
 interface PokemonDao {
-    @Insert(onConflict = OnConflictStrategy.IGNORE)
-    suspend fun insert(pkmn: Pokemon)
-
-    @Update
-    suspend fun update(updatePkmn: Pokemon)
-
-    @Delete
-    suspend fun delete(pkmn: Pokemon)
+    @Query("SELECT * FROM pokemon_table")
+    fun readAllPokemon(): LiveData<List<Pokemon>>
 }
