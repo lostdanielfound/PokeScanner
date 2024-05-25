b75500671082abafb99dbd3e9bbdcb302694e52c
Daniel Guzman <danielsonguzman2000@gmail.com>
2024-05-24 20:49:11 -0400

    Added Init block

--- app/src/main/java/com/example/pokescanner/screens/PokemonViewModel.kt
+++ app/src/main/java/com/example/pokescanner/screens/PokemonViewModel.kt
@@ -1,6 +1,7 @@
 package com.example.pokescanner.screens
 
 import android.app.Application
+import android.content.Context
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
@@ -9,9 +10,17 @@ import com.example.pokescanner.db.PokemonDatabase
 import com.example.pokescanner.db.PokemonRepository
 import kotlinx.coroutines.flow.Flow
 
-class PokemonViewModel(pokemonRepository: PokemonRepository): ViewModel() {
+class PokemonViewModel(application: Application): AndroidViewModel(application) {
 
-    private val _pokemonList = pokemonRepository.readAllPokemon
+    //Need pokemonDao to get access by pokemonRepo
+    private val _pokemonList: Flow<List<Pokemon>>
+    private val pokemonRepository: PokemonRepository
+
+    init {
+        val pokemonDao = PokemonDatabase.getDatabase(application).pokemonDao()
+        pokemonRepository = PokemonRepository(pokemonDao)
+        _pokemonList = pokemonRepository.readAllPokemon
+    }
 
     fun getJournalList(): Flow<List<Pokemon>> {
         return _pokemonList
