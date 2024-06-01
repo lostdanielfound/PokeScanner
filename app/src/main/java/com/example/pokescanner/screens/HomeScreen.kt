bc3406dc9ba47658c7f8759222df4eefabc2c7ac
Daniel Guzman <danielsonguzman2000@gmail.com>
2024-06-01 16:21:10 -0400

    Added needed Camera Controler and preview

--- app/src/main/java/com/example/pokescanner/screens/HomeScreen.kt
+++ app/src/main/java/com/example/pokescanner/screens/HomeScreen.kt
@@ -1,5 +1,6 @@
 package com.example.pokescanner.screens
 
+import androidx.camera.view.CameraController
 import androidx.camera.view.LifecycleCameraController
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -11,19 +12,27 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.material3.rememberBottomSheetScaffoldState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.currentCompositionLocalContext
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
+import com.example.pokescanner.MainActivity
 import com.example.pokescanner.composables.CameraPreview
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen(modifier: Modifier = Modifier) {
     val scaffoldState = rememberBottomSheetScaffoldState()
+    val appContext = LocalContext.current
     val controller = remember {
         //TODO: Find a way to pass applicationContext
-        LifecycleCameraController().apply
+        LifecycleCameraController(appContext).apply {
+            setEnabledUseCases(
+                CameraController.IMAGE_CAPTURE
+            )
+        }
     }
     BottomSheetScaffold(
         scaffoldState = scaffoldState,
@@ -37,7 +46,11 @@ fun HomeScreen(modifier: Modifier = Modifier) {
                 .fillMaxSize()
                 .padding(padding)
         ) {
-            CameraPreview(controller = controller)
+            CameraPreview(
+                controller = controller,
+                modifier = Modifier
+                    .fillMaxSize()
+            )
         }
     }
 }
