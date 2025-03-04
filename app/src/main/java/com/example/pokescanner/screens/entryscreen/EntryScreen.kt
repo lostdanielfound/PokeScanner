cdf0f5a4f8b5e01368eeedbb392239b1b5c9ea18
Daniel Guzman <danielsonguzman2000@gmail.com>
2025-03-04 14:17:40 -0800

    took out some unused imports

--- app/src/main/java/com/example/pokescanner/screens/entryscreen/EntryScreen.kt
+++ app/src/main/java/com/example/pokescanner/screens/entryscreen/EntryScreen.kt
@@ -1,45 +1,25 @@
-package com.example.pokescanner.screens
+package com.example.pokescanner.screens.entryscreen
 
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.CutCornerShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.KeyboardArrowLeft
 import androidx.compose.material.icons.filled.KeyboardArrowLeft
-import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedCard
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.ImageBitmap
-import androidx.compose.ui.graphics.asImageBitmap
-import androidx.compose.ui.graphics.painter.Painter
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.pokescanner.R
 import com.example.pokescanner.composables.DetailedEntryCard
 import com.example.pokescanner.db.NullPokemon
 import com.example.pokescanner.db.Pokemon
-import com.example.pokescanner.utils.BitmapConverter
 
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -49,7 +29,7 @@ fun EntryScreen(
     modifier: Modifier = Modifier
 ) {
     /*
-    https://medium.com/@nosilverbullet/jetpack-compose-suspend-functions-inside-composables-c0ac4568eed4
+    https://medium.com/@nosilverbullet/jetpack-compose-suspend-functions-insit de-composables-c0ac4568eed4
      */
     //Bruh, this stuff should be in it's own composable
     Scaffold(
@@ -59,7 +39,7 @@ fun EntryScreen(
                     Row(
                         verticalAlignment = Alignment.CenterVertically
                     ) {
-                        Icon(imageVector = Icons.Filled.KeyboardArrowLeft, contentDescription = null)
+                        Icon(imageVector = Icons.AutoMirrored.Filled.KeyboardArrowLeft, contentDescription = null)
                         Text(text = loadingPokemon.name)
                     }
                 },
