0cbaa15fd6312a09fd06ca1e24ac103f02c1bb37
lostdanielfound <daniellostsilver2000@gmail.com>
2025-03-04 20:58:29 -0800

    started working on EntryViewModel

--- app/src/main/java/com/example/pokescanner/screens/entryscreen/EntryViewmodel.kt
+++ app/src/main/java/com/example/pokescanner/screens/entryscreen/EntryViewmodel.kt
@@ -3,4 +3,8 @@ package com.example.pokescanner.screens.entryscreen
 import androidx.lifecycle.ViewModel
 
 class EntryViewmodel(): ViewModel() {
+    private var _entryState = MutableStateFlow(EntryState())
+    private val entryState = _entryState.asStateFlow()
+
+
 }
