12be5d39867d561c947b0e269ae5547c91588828
lostdanielfound <daniellostsilver2000@gmail.com>
2025-03-05 14:26:43 -0800

    Change function header

--- app/src/main/java/com/example/pokescanner/screens/journalscreen/JournalScreen.kt
+++ app/src/main/java/com/example/pokescanner/screens/journalscreen/JournalScreen.kt
@@ -3,19 +3,25 @@ package com.example.pokescanner.screens.journalscreen
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.pokescanner.composables.EntryCardRow
 import com.example.pokescanner.db.Pokemon
 
 @Composable
 fun JournalScreen(
-    pokemonList: List<Pokemon>,
-    entryOnClick: (Int) -> Unit,
-    modifier: Modifier = Modifier
+    onEntryClick: (Int) -> Unit,
+    modifier: Modifier = Modifier,
+    journalViewmodel: JournalViewmodel = viewModel()
 ) {
+    //Upon Viewmodel init, you should be able to get the pokemon list from the db.
+    val journalState = journalViewmodel.journalState.collectAsState()
+    val pokemonList = journalViewmodel.getPokemonList().collectAsState(initial = emptyList()).value
+
     LazyColumn(modifier = modifier) {
         items(pokemonList) { entry ->
-            EntryCardRow(pkmn = entry, onClick = entryOnClick)
+            EntryCardRow(pkmn = entry, onClick = onEntryClick)
         }
     }
 }
