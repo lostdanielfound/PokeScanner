a91d2c34133313e2f3c7677416afabf57b1f3022
lostdanielfound <daniellostsilver2000@gmail.com>
2025-03-05 18:26:37 -0800

    Set _private viewmodel state and removed unused imports

--- app/src/main/java/com/example/pokescanner/screens/journalscreen/JournalViewmodel.kt
+++ app/src/main/java/com/example/pokescanner/screens/journalscreen/JournalViewmodel.kt
@@ -2,12 +2,9 @@ package com.example.pokescanner.screens.journalscreen
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
-import com.example.pokescanner.db.NullPokemon
 import com.example.pokescanner.db.Pokemon
 import com.example.pokescanner.db.PokemonDatabase
 import com.example.pokescanner.db.PokemonRepository
-import com.example.pokescanner.screens.AppUiState
-import com.example.pokescanner.screens.statsscreen.Stats
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -15,7 +12,7 @@ import kotlinx.coroutines.flow.asStateFlow
 
 class JournalViewmodel(application: Application): AndroidViewModel(application) {
 
-    private val _journalState = MutableStateFlow(JournalState())
+    private val _journalState = MutableStateFlow(JournalState(temp = 0))
     val journalState: StateFlow<JournalState> = _journalState.asStateFlow()
 
     private val pokemonList: Flow<List<Pokemon>> // Flow object that fills list of pokemon objects
