95009c25b591893121f8e7dddb1b170863eb19d6
Daniel Guzman <danielsonguzman2000@gmail.com>
2025-03-06 13:57:43 -0800

    added module annotation

--- app/src/main/java/com/example/pokescanner/singleton/Appmodule.kt
+++ app/src/main/java/com/example/pokescanner/singleton/Appmodule.kt
@@ -3,12 +3,14 @@ package com.example.pokescanner.singleton
 import android.content.Context
 import com.example.pokescanner.db.PokemonDatabase
 import com.example.pokescanner.db.PokemonRepository
+import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
+@Module
 @InstallIn(SingletonComponent::class)
 object Appmodule {
 
