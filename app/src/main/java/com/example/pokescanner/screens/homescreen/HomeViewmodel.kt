a9dbe5debc06897725c90bfa9331c2d7f05aad7c
lostdanielfound <daniellostsilver2000@gmail.com>
2025-03-05 18:31:13 -0800

    Created and organized initital viewmodel

--- app/src/main/java/com/example/pokescanner/screens/homescreen/HomeViewmodel.kt
+++ app/src/main/java/com/example/pokescanner/screens/homescreen/HomeViewmodel.kt
@@ -2,11 +2,14 @@ package com.example.pokescanner.screens.homescreen
 
 import android.graphics.Bitmap
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 
 class HomeViewmodel(): ViewModel() {
 
-    private var _homeState = MutableStateFlow(homeState())
-    private val homeState = _homeState.asStateFlow()
+    private val _homeState = MutableStateFlow(HomeState(temp = 0))
+    val homeState: StateFlow<HomeState> = _homeState.asStateFlow()
     /**
      * onPhotoTaken will process the respective image as a bitmap
      * through a pokemon classification model to make the best prediction of the pokemon
@@ -19,7 +22,7 @@ class HomeViewmodel(): ViewModel() {
 
     }
 
-    resetState() {
-        _homeState.value = homeState()
+    fun resetState() {
+        _homeState.value = HomeState(temp = 0)
     }
 }
