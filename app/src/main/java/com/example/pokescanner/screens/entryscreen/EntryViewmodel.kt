08ac0bab85c4973056020f7c3dfbbb76f81c468c
Daniel Guzman <danielsonguzman2000@gmail.com>
2025-03-06 13:10:35 -0800

    Added function phototype to get pokemon object from db, state holds pokemon object

--- app/src/main/java/com/example/pokescanner/screens/entryscreen/EntryViewmodel.kt
+++ app/src/main/java/com/example/pokescanner/screens/entryscreen/EntryViewmodel.kt
@@ -1,12 +1,24 @@
 package com.example.pokescanner.screens.entryscreen
 
 import androidx.lifecycle.ViewModel
+import com.example.pokescanner.db.NullPokemon
+import com.example.pokescanner.db.Pokemon
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 
 class EntryViewmodel(): ViewModel() {
-    private val _entryState = MutableStateFlow(EntryState(temp = 0))
+    private val _entryState = MutableStateFlow(EntryState(pokemon = NullPokemon.getNullPokemon()))
     val entryState = _entryState.asStateFlow()
 
-    fun
+    /**
+     * Function will be used during init of view model to initalizing the UI state with the loaded
+     * pokemon that was selected from the JournalScreen
+     *
+     * @param pokemonId The ID of the pokemon that was select from the JournalScreen Nav composable
+     */
+    private fun getPokemonId(pokemonId: Int): Pokemon {
+
+        //TODO: Design the process of querying the database
+        return NullPokemon.getNullPokemon()
+    }
 }
