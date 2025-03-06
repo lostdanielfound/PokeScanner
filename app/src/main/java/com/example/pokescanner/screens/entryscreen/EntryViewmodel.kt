726916e4e33f0357e666784d327f09ada45c1fc3
lostdanielfound <daniellostsilver2000@gmail.com>
2025-03-05 18:37:12 -0800

    idk just changed something

--- app/src/main/java/com/example/pokescanner/screens/entryscreen/EntryViewmodel.kt
+++ app/src/main/java/com/example/pokescanner/screens/entryscreen/EntryViewmodel.kt
@@ -1,10 +1,12 @@
 package com.example.pokescanner.screens.entryscreen
 
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 
 class EntryViewmodel(): ViewModel() {
-    private var _entryState = MutableStateFlow(EntryState())
-    private val entryState = _entryState.asStateFlow()
+    private val _entryState = MutableStateFlow(EntryState(temp = 0))
+    val entryState = _entryState.asStateFlow()
 
-
+    fun
 }
