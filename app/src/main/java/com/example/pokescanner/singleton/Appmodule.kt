3ee33b19c3a53009f09d478605c839480e67ec66
Daniel Guzman <danielsonguzman2000@gmail.com>
2025-03-06 13:32:00 -0800

    Provided hilt app context to get database repo singleton

--- app/src/main/java/com/example/pokescanner/singleton/Appmodule.kt
+++ app/src/main/java/com/example/pokescanner/singleton/Appmodule.kt
@@ -1,17 +1,22 @@
 package com.example.pokescanner.singleton
 
+import android.content.Context
 import com.example.pokescanner.db.PokemonDatabase
 import com.example.pokescanner.db.PokemonRepository
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @InstallIn(SingletonComponent::class)
-abstract class Appmodule {
+object Appmodule {
+
     @Provides
-    fun providesDatabaseRepository(): PokemonRepository {
+    @Singleton
+    fun providesDatabaseRepository(@ApplicationContext applicationContext: Context): PokemonRepository {
         //TODO need to figure out how to pass in the Application context
-        val pokemonDao = PokemonDatabase.getDatabase(ApplicationContext).pokemonDao()
+        val pokemonDao = PokemonDatabase.getDatabase(context = applicationContext).pokemonDao()
         return PokemonRepository(pokemonDao)
     }
 }
