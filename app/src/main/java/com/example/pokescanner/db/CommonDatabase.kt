bfd00461777a3cfa649a2e02362fe6bf129a502b
lostdanielfound <daniellostsilver2000@gmail.com>
2025-03-19 23:44:19 -0700

    deleted comment

--- app/src/main/java/com/example/pokescanner/db/CommonDatabase.kt
+++ app/src/main/java/com/example/pokescanner/db/CommonDatabase.kt
@@ -8,32 +8,4 @@ abstract class CommonDatabase: RoomDatabase() {
 
     abstract fun capturedPkmnDao(): CapturedPkmnDao
     abstract fun playerStatsDao(): PlayerStatsDao
-
-    /*
-    companion object{
-        @Volatile //Creating an atomic Instance of the database
-        private var INSTANCE: CapturedPkmnDatabase? = null
-
-        //Function to get and start building process of database
-        //If instance variable is already set with current state
-        //of database we will return that since building a database
-        //is quite expensive for performance.
-        fun getDatabase(context: Context): CapturedPkmnDatabase{
-            val tempInstance = INSTANCE
-            if (tempInstance != null) {
-                return tempInstance
-            }
-            synchronized(this) {
-                val instance = Room.databaseBuilder(
-                    context.applicationContext,
-                    CapturedPkmnDatabase::class.java,
-                    "CapturedPkmn_database"
-                ).build()
-                INSTANCE = instance
-                return instance
-            }
-        }
-    }
-
-     */
 }
