d88e95a9dda87d830c76784649728143463b36f2
lostdanielfound <daniellostsilver2000@gmail.com>
2025-03-23 16:57:40 -0700

    Added repository usecases

--- app/src/main/java/com/example/pokescanner/screens/statsscreen/StatsViewmodel.kt
+++ app/src/main/java/com/example/pokescanner/screens/statsscreen/StatsViewmodel.kt
@@ -1,29 +1,36 @@
 package com.example.pokescanner.screens.statsscreen
 
+import android.util.Log
 import androidx.compose.runtime.collectAsState
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.pokescanner.db.PlayerStats
 import com.example.pokescanner.db.PlayerStatsRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class StatsViewmodel @Inject constructor (
     playerStatsImpl: PlayerStatsRepository
 ): ViewModel() {
-    private val _statsState = MutableStateFlow(Stats(
-        totalImagesTaken = 0,
-        totalPkmnCaptured = 0,
-        totalPkmnEntriesCompleted = 0
-    ))
-    val statsState: StateFlow<Stats> = _statsState.asStateFlow()
+    private val _statsState = MutableStateFlow<Stats?>(null)
+    val statsState: StateFlow<Stats?> = _statsState.asStateFlow()
 
-    /**
-     * Resets the stats and makes a query to obtain stats
-     */
-    private fun resetState() {
-
+    init {
+        Log.d("StatsViewmodel", "StatsViewmodel init")
+        viewModelScope.launch(Dispatchers.IO) {
+            playerStatsImpl.readPlayerStats.collect { playerStats ->
+                _statsState.update {
+                    playerStats.toStats()
+                }
+            }
+        }
     }
 }
