75e7a6ae59d3bb53a9b89747aeb17e99d4d1a3a2
lostdanielfound <daniellostsilver2000@gmail.com>
2025-03-23 10:48:51 -0700

    change exportSchema para to false

--- app/src/main/java/com/example/pokescanner/db/CommonDatabase.kt
+++ app/src/main/java/com/example/pokescanner/db/CommonDatabase.kt
@@ -3,7 +3,7 @@ package com.example.pokescanner.db
 import androidx.room.Database
 import androidx.room.RoomDatabase
 
-@Database(entities = [CapturedPkmn::class, PlayerStats::class], version = 1, exportSchema = true)
+@Database(entities = [CapturedPkmn::class, PlayerStats::class], version = 1, exportSchema = false)
 abstract class CommonDatabase: RoomDatabase() {
 
     abstract fun capturedPkmnDao(): CapturedPkmnDao
--- app/src/main/java/com/example/pokescanner/db/PokemonDatabase.kt
+++ app/src/main/java/com/example/pokescanner/db/PokemonDatabase.kt
@@ -5,31 +5,8 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
-@Database(entities = [Pokemon::class], version = 1, exportSchema = true)
+@Database(entities = [Pokemon::class], version = 1, exportSchema = false)
 abstract class PokemonDatabase: RoomDatabase() {
 
     abstract fun pokemonDao(): PokemonDao
-/*
-    companion object {
-        @Volatile
-        private var INSTANCE: PokemonDatabase? = null
-
-        fun getDatabase(context: Context): PokemonDatabase {
-            val tempInstance = INSTANCE
-            if (tempInstance != null) {
-                return tempInstance
-            }
-            synchronized(this) {
-                val instance = Room.databaseBuilder(
-                    context.applicationContext,
-                    PokemonDatabase::class.java,
-                    "pokemon_database"
-                ).createFromAsset("database/pokemon.db").build()
-                INSTANCE = instance
-                return instance
-            }
-        }
-    }
-
- */
 }
